3ffb6c7351dc4903a9059a1d0697c5a7cf1a4d8c
Max Nikitin <mr.maxnikitin@yandex.ru>
2017-01-05 19:28:04 +0300

    robotx.txt и метрика

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -8,9 +8,9 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Статистика" do
           yandex_link = <<-LINK
             <!-- Yandex.Metrika informer -->
-<a href="https://metrika.yandex.ru/stat/?id=41916344&amp;from=informer"
-target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/41916344/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="41916344" data-lang="ru" /></a>
+<a href="https://metrika.yandex.ru/stat/?id=41957529&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/41957529/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="41957529" data-lang="ru" /></a>
 <!-- /Yandex.Metrika informer -->
           LINK
           yandex_link.html_safe
