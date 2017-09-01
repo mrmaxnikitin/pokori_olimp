a7fc7799e54051074fc7dc787233ce77095c0de2
Max Nikitin <mr.maxnikitin@yandex.ru>
2017-09-01 22:59:51 +0300

    ККС

--- app/assets/javascripts/my_scripts.js
+++ app/assets/javascripts/my_scripts.js
@@ -9,4 +9,22 @@ $(document).on("page:load ready", function main(){
 	    $('.payment-form').css('display', 'block');
 	  });
 
+	  $('.custEmail').change(function(){
+			custEmail = $('.custEmail').val()
+    	  		$('.ym_merchant_receipt').val('{\"customerContact\":\"'+custEmail+'\",\"taxSystem\": 2,\"items\": [{\"quantity\": 1,\"price\": {\"amount\": 1.00},\"tax\": 1,\"text\": \"Услуги по обработке данных\"}]}')
+	 	});
+	 	
+	 	$('.custEmail').change(function(){
+		        if($(this).val()!=''){
+		            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+		            if(pattern.test($(this).val())){
+		                $('#getpay').prop('disabled', false)
+		            } else {
+		                ('#getpay').prop('disabled', true)
+		            }
+		        }else{
+		            $('#getpay').prop('disabled', true)
+		        }
+		});
+
 });
